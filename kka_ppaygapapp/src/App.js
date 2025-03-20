2f4f3121ba17923e1583064c39a6777408c73b9a
ktyler05 <tylerk@cardiff.ac.uk>
2025-03-20 09:14:57 +0000

    Basic set up
    - needs some changes though

---
+++ kka_ppaygapapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Quiz from './Quiz.js';
+import './style.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gender Pay Gap Quiz</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
