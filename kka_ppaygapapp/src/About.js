5df0482d58ffd8e9c68388eb7c859e343cdeb146
ktyler05 <tylerk@cardiff.ac.uk>
2025-03-27 12:26:59 +0000

    contact, about updates
    added some bare-bones content to these pages

--- kka_ppaygapapp/src/About.js
+++ kka_ppaygapapp/src/About.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './style.css'; 
+import './style.css';
 
 function About() {
   return (
     <div id="quiz-container" className="quiz-container start-screen">
       <h2>About</h2>
-      <p>This is the About page. </p>
+      <p>
+        This is a project created by Kim, Alice & Kendra for the Digital
+        Investigation module for Cardiff Universities Computational and Data
+        Journalism Masters course. Our aim in creating this project was to
+        educate people about the gender pay gap and how, despite current popular
+        beliefs, it is still prevelant within our society.{" "}
+      </p>
     </div>
   );
 }
--- kka_ppaygapapp/src/Contact.js
+++ kka_ppaygapapp/src/Contact.js
@@ -6,7 +6,13 @@ function Contact() {
     <div id="quiz-container" className="quiz-container start-screen">
 
       <h2>Contact Us</h2>
-      <p>This is the Contact Us page.</p>
+      <p>
+        You can contact us at:{" "}
+        <a href="mailto:KendraAliceTyler@gmail.com">KendraAliceTyler@gmail.com</a> or on LinkedIn at{" "}
+        <a href="https://www.linkedin.com/in/kim-anne/" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      </p>
     </div>
   );
 }
